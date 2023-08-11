fd2afd9d74cd657f088b675cafbd073ab741b441
moreajay <mac.show24@gmail.com>
2023-08-12 01:07:09 +0530

    test

---
+++ style.js
@@ -0,0 +1,50 @@
+
+// var a=20;  
+// if(a>10){  
+//    document.getElementById("demo").style.display = "block";
+//   }
+//   else{
+
+//     document.getElementById("test").style.display= "block";
+  
+//   }
+
+
+
+console.log("Hello, World!");
+
+const time = new Date();
+const day = time.toLocaleString("en-us", {weekday: "long"});
+const morning = time.getHours() >=6 && time.getHours ()<=12;
+
+let dayMessage;
+
+if(day.toLocaleLowerCase()==="monday"){
+
+    dayMessage = `Happy ${day}`;
+}
+else if (day.toLocaleLowerCase()==="tuesday"){
+
+   dayMessage = `Happy ${day}, Four days tp go`;
+}
+else if (day.toLocaleLowerCase()==="wednesday"){
+
+   dayMessage = `Happy ${day}, Half way there`;
+}
+else if (day.toLocaleLowerCase()==="thursday"){
+
+   dayMessage = `Happy ${day}, Start planing for weekned`;
+}
+else if (day.toLocaleLowerCase()==="friday"){
+
+   dayMessage = `Happy ${day}, Cheers weekend is coming`;
+}
+else if (day.toLocaleLowerCase()==="saturday"){
+
+   dayMessage = `Happy ${day}, Hola its weekned`;
+}
+
+console.log(dayMessage);
+
+document.getElementById("myP").innerHTML = dayMessage;
+document.getElementById("sat").style.display ="block";
